4ec9ebe1cb7a688305981b9803bd576c92cbe4e3
Sergey Ermakov <sergey.a.ermakov@gmail.com>
2018-07-14 13:27:14 +0300

    changes from lint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import readlineSync from 'readline-sync';
 
 const askquestion = () => {
-const actual = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${actual}!`);
-}
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
+};
 
 export default askquestion;
