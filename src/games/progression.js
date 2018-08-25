22feb26a8831a461fda428cdaebf7493be0de6ee
Sergey Ermakov <sergey.a.ermakov@gmail.com>
2018-08-25 10:34:15 +0300

    add game progression version 2

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,26 +2,9 @@ import { cons } from 'hexlet-pairs';
 import getRandomNum from '../common';
 import playRound from '../gametemplate';
 
-// first version
-const getProgression = (a, b) => {
-  let s = '';
-  let p = a;
-  let answer;
-  for (let i = 0; i < 10; i += 1) {
-    p = a + b * i;
-    if (i === 5) {
-      answer = p;
-      p = '..';
-    }
-    s = `${s} ${p.toString()}`;
-  }
-  return [answer, s];
-};
-
 const isUndefined = s => (s === undefined);
 
-// second version
-const getProgression2 = (a, b, i, count, acc) => {
+const getProgression = (a, b, i, count, acc) => {
   if (count === 0) return acc;
   const nextMember = a + b;
   let result = [acc[0], isUndefined(acc[1]) ? '' : `${acc[1]} `];
@@ -31,7 +14,7 @@ const getProgression2 = (a, b, i, count, acc) => {
     result[1] = `${result[1]}${nextMember.toString()}`;
   }
 
-  return getProgression2(nextMember, b, i, count - 1, result);
+  return getProgression(nextMember, b, i, count - 1, result);
 };
 
 const description = 'What number is missing in this progression?';
@@ -39,7 +22,7 @@ const description = 'What number is missing in this progression?';
 const gameData = () => {
   const number1 = getRandomNum(1, 20);
   const number2 = getRandomNum(1, 20);
-  const [rightAnswer, question] = getProgression2(number1, number2, 5, 10, []);
+  const [rightAnswer, question] = getProgression(number1, number2, 5, 10, []);
   return cons(question, rightAnswer.toString());
 };
 
