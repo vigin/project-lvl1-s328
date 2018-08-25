63d1e92df2642b846d0fba335accb679abdf3551
Sergey Ermakov <sergey.a.ermakov@gmail.com>
2018-08-25 14:27:03 +0300

    changed game progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,19 +2,18 @@ import { cons } from 'hexlet-pairs';
 import getRandomNum from '../common';
 import playRound from '../gametemplate';
 
-const getProgression = (a, b) => {
-  let s = '';
-  let p;
-  let answer;
+const getMemberProgression = (start, step, index) => start + step * index;
+
+const getProgression = (start, step) => {
+  let result = '';
   for (let i = 0; i < 10; i += 1) {
-    p = a + b * i;
-    if (i === 5) {
-      answer = p;
-      p = '..';
+    if (i !== 5) {
+      result = `${result} ${getMemberProgression(start, step, i).toString()}`;
+    } else {
+      result = `${result} ..`;
     }
-    s = `${s} ${p.toString()}`;
   }
-  return [answer, s];
+  return result;
 };
 
 const description = 'What number is missing in this progression?';
@@ -22,7 +21,8 @@ const description = 'What number is missing in this progression?';
 const gameData = () => {
   const number1 = getRandomNum(1, 20);
   const number2 = getRandomNum(1, 20);
-  const [rightAnswer, question] = getProgression(number1, number2);
+  const question = getProgression(number1, number2);
+  const rightAnswer = getMemberProgression(number1, number2, 5);
   return cons(question, rightAnswer.toString());
 };
 
